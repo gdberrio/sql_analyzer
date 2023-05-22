6660b307f814aa85f1aa84fb2ffa52f53809c7b1
Guilherme Diaz-Berrio <guilhermediaz-berrio@gmail.com>
2023-05-22 23:03:59 +0100

    formatting

--- schema_explorer.py
+++ schema_explorer.py
@@ -7,30 +7,34 @@ import urllib
 
 load_dotenv()
 
-server = getenv('SERVER_DB')
-database = getenv('DATABASE')
-username = getenv('USER_DB')
-password = getenv('PASSWORD_DB')
+server = getenv("SERVER_DB")
+database = getenv("DATABASE")
+username = getenv("USER_DB")
+password = getenv("PASSWORD_DB")
+
 
 def get_engine() -> Engine:
-    params = urllib.parse.quote_plus(r'Driver={ODBC Driver 18 for SQL Server};'
-                                     r'Server=tcp:contoso-tests.database.windows.net,1433;'
-                                     r'Database=Contoso-DW;'
-                                     r'Uid=admin-gdb;'
-                                     rf'Pwd={password};'
-                                     r'Encrypt=yes;'
-                                     r'TrustServerCertificate=no;'
-                                     r'Connection Timeout=30;')
+    params = urllib.parse.quote_plus(
+        r"Driver={ODBC Driver 18 for SQL Server};"
+        r"Server=tcp:contoso-tests.database.windows.net,1433;"
+        r"Database=Contoso-DW;"
+        r"Uid=admin-gdb;"
+        rf"Pwd={password};"
+        r"Encrypt=yes;"
+        r"TrustServerCertificate=no;"
+        r"Connection Timeout=30;"
+    )
 
-    engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}") 
+    engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
     return engine
 
-def get_metadata(engine: Engine, schema: str) -> MetaData:
 
+def get_metadata(engine: Engine, schema: str) -> MetaData:
     metadata = MetaData()
 
     metadata.reflect(bind=engine, schema=schema)
     return metadata
+
 
 def get_samples(engine: Engine, metadata: MetaData) -> Dict[str, List[ResultProxy]]:
     samples = dict()
