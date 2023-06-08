b2015fd99547a3515ff357cab09cc70939cb6ec5
Guilherme Diaz-Berrio <guilhermediaz-berrio@gmail.com>
2023-06-08 09:03:07 +0100

    reflection and samples work on pagila. Not on adventure works due to XML column types

--- schema_explorer.py
+++ schema_explorer.py
@@ -1,16 +1,17 @@
-from typing import Dict, List
-from sqlalchemy import Engine, MetaData, ResultProxy, create_engine, inspect
-from sqlalchemy.orm import Session
+from typing import Dict
+from sqlalchemy import Engine, MetaData, create_engine, inspect
 from sqlalchemy.schema import CreateTable
+from sqlalchemy.engine.reflection import Inspector
 from dotenv import load_dotenv
 from os import getenv
 from dataclasses import dataclass
 from enum import Enum
 import logging
 import csv
+import pandas as pd
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 server = getenv("SERVER_DB")
 database = getenv("DATABASE")
@@ -18,8 +19,10 @@ port = getenv("PORT_DB")
 username = getenv("USER_DB")
 password = getenv("PASSWORD_DB")
 
+
 class Database(str, Enum):
-    PostgreSQL = 'postgresql'
+    PostgreSQL = "postgresql"
+
 
 @dataclass
 class DBConfig:
@@ -28,26 +31,27 @@ class DBConfig:
     database: str
     port: str
     username: str
-    password: str 
+    password: str
+
 
 def get_engine(DB: DBConfig) -> Engine:
     """
-        Constructs a SQLAlchemy engine instance that establishes a connection to the specified database.
+    Constructs a SQLAlchemy engine instance that establishes a connection to the specified database.
 
-        Args:
-            DB (DBConfig): An instance of the DBConfig dataclass which includes necessary database parameters.
+    Args:
+        DB (DBConfig): An instance of the DBConfig dataclass which includes necessary database parameters.
 
-        Returns:
-            Engine: SQLAlchemy engine instance that's connected to the specified database.
+    Returns:
+        Engine: SQLAlchemy engine instance that's connected to the specified database.
     """
 
     # TODO: generalize this to allow for selection of DB, and correct selection of Driver and connection string
     if DB is None:
-        DB = DBConfig(db_type=Database.PostgreSQL, server=server, database=database, port=port, username=username, password=password) # type: ignore
+        DB = DBConfig(db_type=Database.PostgreSQL, server=server, database=database, port=port, username=username, password=password)  # type: ignore
 
     connection_url = f"{DB.db_type.value}://{DB.username}:{DB.password}@{DB.server}:{DB.port}/{DB.database}"
     engine = create_engine(connection_url)
-    logging.info('Engine created: %s', repr(engine.url))
+    logging.info("Engine created: %s", repr(engine.url))
     return engine
 
 
@@ -66,11 +70,11 @@ def get_metadata(engine: Engine, schema: str) -> MetaData:
     metadata = MetaData()
 
     metadata.reflect(bind=engine, schema=schema)
-    logging.info('Metadata object created: %s', metadata)
+    logging.info("Metadata object created: %s", metadata)
     return metadata
 
 
-def get_samples(engine: Engine, metadata: MetaData) -> Dict[str, List[ResultProxy]]:
+def get_samples(engine: Engine, metadata: MetaData, schema: str) -> Dict[str, str]:
     """
     Fetches a sample of data (10 rows) from each table of the connected database.
 
@@ -83,16 +87,21 @@ def get_samples(engine: Engine, metadata: MetaData) -> Dict[str, List[ResultProx
         each containing the data of 10 rows from the corresponding table.
     """
 
-    samples = dict()
+    processed_samples = dict()
     for table in metadata.tables.values():
-        session = Session(engine)
-        results = session.query(table).limit(10).all()
-        samples[table.name] = results
-        session.close()
-    return samples
+        logging.info("for schema %s, table %s, connecting:", schema, table.name)
+        table_sample_generator = pd.read_sql_table(
+            table.name, con=engine, schema=schema, chunksize=10
+        )
+        logging.info("obtained generator.")
+        table_sample = next(table_sample_generator)  # type: ignore
+        table_sample.to_csv(f"data/{table}.csv", index=False)
+        processed_samples[table] = table_sample
+        logging.info("Saved %s table sample to %s.csv", table, table)
+    return processed_samples
 
 
-def store_schema_details(engine: Engine, metadata: MetaData):  
+def store_schema_details(engine: Engine):
     """
     Stores details of each table in the connected database, including column names, column types,
     and a sample of data, and writes the SQL table creation statement into a file.
@@ -107,32 +116,23 @@ def store_schema_details(engine: Engine, metadata: MetaData):
         None
     """
 
-    with open('data/table_definitions.sql', 'w') as sql_file:
-        for table in metadata.tables.values():
-            sql_file.write(str(CreateTable(table).compile(engine)))
-            sql_file.write(';\n')
+    inspector = Inspector.from_engine(engine)
+    schemas = inspector.get_schema_names()
+    for schema in schemas:
+        logging.info("Entering schema: %s", schema)
+        metadata = get_metadata(engine=engine, schema=schema)
+        with open(f"data/{schema}_table_definitions.sql", "w") as sql_file:
+            for table in metadata.tables.values():
+                sql_file.write(str(CreateTable(table).compile(engine)))
+                sql_file.write(";\n")
 
-        logging.info('Saved metadata into file %s', sql_file)
+        logging.info("Saved metadata for schema %s into file %s", schema, sql_file)
 
-    samples = get_samples(engine=engine, metadata=metadata)
-    for table_name, results in samples.items():
-        if results:  # check if there are results for the table
-            # Open the CSV file in write mode
-            with open(f'data/{table_name}.csv', 'w', newline='') as csv_file:
-                writer = csv.writer(csv_file)
+        logging.info("Getting samples from each table in schema %s", schema)
+        get_samples(engine=engine, metadata=metadata, schema=schema)
 
-                # Write the header
-                inspector = inspect(engine)
-                columns = inspector.get_columns(table_name)
-                writer.writerow([column['name'] for column in columns])
-
-                # Iterate over the query results and write each row to the CSV
-                for row in results:
-                    writer.writerow([getattr(row, column['name']) for column in columns])
-            logging.info('Saved %s table sample to %s', table_name, csv_file)
 
 if __name__ == "__main__":
-    DB = DBConfig(db_type=Database.PostgreSQL, server=server, database=database, port=port, username=username, password=password) # type: ignore
+    DB = DBConfig(db_type=Database.PostgreSQL, server=server, database=database, port=port, username=username, password=password)  # type: ignore
     engine = get_engine(DB=DB)
-    metadata = get_metadata(engine=engine, schema='public')
-    store_schema_details(engine=engine, metadata=metadata)
+    store_schema_details(engine=engine)
